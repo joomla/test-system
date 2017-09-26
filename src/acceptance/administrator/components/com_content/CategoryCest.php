ed6d28c80973bfdbcac063838b608085653488bf
puneet0191 <puneet.0191@gmail.com>
2017-09-26 20:41:06 +0530

    Category Cest File

---
+++ src/acceptance/administrator/components/com_content/CategoryCest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @package     Joomla.Tests
+ * @subpackage  Acceptance.tests
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+use Step\Acceptance\Administrator\Category as CategoryStep;
+use Page\Acceptance\Administrator\CategoryManagerPage;
+
+class CategoryCest
+{
+	public function __construct()
+	{
+		$this->faker = Faker\Factory::create();
+		$this->articleTitle = "Article One";
+		$this->randomCategoryTitle = $this->faker->bothify('CategoryCest ?##?');
+	}
+
+	public function _before(AcceptanceTester $I)
+	{
+		$I->doAdministratorLogin();
+	}
+
+	public function Category(AcceptanceTester $I, $scenario)
+	{
+		$I = new CategoryStep($scenario);
+		$I->createCategory($this->randomCategoryTitle);
+	}
+}
