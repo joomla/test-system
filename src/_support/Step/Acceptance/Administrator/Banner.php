3d7fd7a36e552b09c9e1fdcdd7b6cf761ada5658
puneet0191 <puneet.0191@gmail.com>
2017-09-26 16:47:19 +0530

    Adding Step Classes

---
+++ src/_support/Step/Acceptance/Administrator/Banner.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * @package     Joomla.Test
+ * @subpackage  AcceptanceTester.Step
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Step\Acceptance\Administrator;
+
+use Page\Acceptance\Administrator\BannerManagerPage;
+
+/**
+ * Acceptance Step object class contains suits for Content Manager.
+ *
+ * @package  Step\Acceptance\Administrator
+ *
+ * @since    __DEPLOY_VERSION__
+ */
+class Banner extends Admin
+{
+
+	public function createBanner($title, $message)
+	{
+		$I = $this;
+		$I->amOnPage(BannerManagerPage::$url);
+		$I->clickToolbarButton('New');
+		$I->fillField(BannerManagerPage::$titleField, $title);
+		$I->clickToolbarButton('Save & Close');
+		$I->assertSuccessMessage($message);
+	}
+
+	public function assertSuccessMessage($message)
+	{
+		$I = $this;
+		$I->waitForText($message, TIMEOUT, BannerManagerPage::$systemMessageContainer);
+		$I->see($message, BannerManagerPage::$systemMessageContainer);
+	}
+
+	public function modifyBanner($bannerTitle, $updatedTitle, $message)
+	{
+		$I = $this;
+		$I->amOnPage(BannerManagerPage::$url);
+		$I->fillField(BannerManagerPage::$searchField, $bannerTitle);
+		$I->Click(BannerManagerPage::$searchButton);
+		$I->checkAllResults();
+		$I->clickToolbarButton('edit');
+		$I->waitForElement(BannerManagerPage::$titleField, TIMEOUT);
+		$I->fillField(BannerManagerPage::$titleField, $updatedTitle);
+		$I->fillField(BannerManagerPage::$aliasField, $updatedTitle);
+		$I->clickToolbarButton('Save & Close');
+		$I->assertSuccessMessage($message);
+	}
+
+	public function publishBanner($bannerTitle, $message)
+	{
+		$I = $this;
+		$I->amOnPage(BannerManagerPage::$url);
+		$I->waitForElement(BannerManagerPage::$searchField, TIMEOUT);
+		$I->fillField(BannerManagerPage::$searchField, $bannerTitle);
+		$I->Click(BannerManagerPage::$searchButton);
+		$I->checkAllResults();
+		$I->clickToolbarButton('Publish');
+		$I->assertSuccessMessage($message);
+	}
+
+	public function unpublishBanner($bannerTitle, $message)
+	{
+		$I = $this;
+		$I->amOnPage(BannerManagerPage::$url);
+		$I->waitForElement(BannerManagerPage::$searchField, TIMEOUT);
+		$I->fillField(BannerManagerPage::$searchField, $bannerTitle);
+		$I->Click(BannerManagerPage::$searchButton);
+		$I->checkAllResults();
+		$I->clickToolbarButton('Unpublish');
+		$I->assertSuccessMessage($message);
+	}
+
+	public function checkInBanner($bannerTitle, $message)
+	{
+		$I = $this;
+		$I->amOnPage(BannerManagerPage::$url);
+		$I->waitForElement(BannerManagerPage::$searchField, TIMEOUT);
+		$I->fillField(BannerManagerPage::$searchField, $bannerTitle);
+		$I->Click(BannerManagerPage::$searchButton);
+		$I->checkAllResults();
+		$I->clickToolbarButton('Check-in');
+		$I->assertSuccessMessage($message);
+	}
+
+	public function trashBanner($bannerTitle, $message)
+	{
+		$I = $this;
+		$I->amOnPage(BannerManagerPage::$url);
+		$I->waitForElement(BannerManagerPage::$searchField, TIMEOUT);
+		$I->fillField(BannerManagerPage::$searchField, $bannerTitle);
+		$I->Click(BannerManagerPage::$searchButton);
+		$I->checkAllResults();
+		$I->clickToolbarButton('Trash');
+		$I->assertSuccessMessage($message);
+	}
+
+	public function deleteBanner($bannerTitle, $message)
+	{
+		$I = $this;
+		$I->amOnPage(BannerManagerPage::$url);
+		$I->waitForElement(BannerManagerPage::$searchField, TIMEOUT);
+		$I->selectOptionInChosenByIdUsingJs('filter_published', "Trashed");
+		$I->fillField(BannerManagerPage::$searchField, $bannerTitle);
+		$I->Click(BannerManagerPage::$searchButton);
+		$I->checkAllResults();
+		$I->clickToolbarButton('Empty trash');
+		$I->acceptPopup();
+	}
+}
