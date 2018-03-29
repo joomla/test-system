20fa38a87f10ca861215c87c81de3a7c43707312
David Neukirchen <david.neukirchen@gmail.com>
2018-03-29 19:05:49 +0200

    Create login page object

---
+++ src/_support/Page/Acceptance/Administrator/LoginPage.php
@@ -0,0 +1,43 @@
+<?php namespace Page\Acceptance\Administrator;
+
+/**
+ * Acceptance Page object class for admin login.
+ *
+ * @package  Page\Acceptance\Administrator
+ *
+ * @since    __DEPLOY_VERSION__
+ */
+class LoginPage
+{
+	/**
+	 * Locator for the Login Page Url
+	 *
+	 * @var    string
+	 * @since  __DEPLOY_VERSION__
+	 */
+	public static $url = '/administrator/index.php';
+
+	/**
+	 * Locator for the login form
+	 *
+	 * @var    array
+	 * @since  __DEPLOY_VERSION__
+	 */
+	public static $form = ['form-login'];
+
+	/**
+	 * Locator for the username field
+	 *
+	 * @var    array
+	 * @since  __DEPLOY_VERSION__
+	 */
+	public static $userNameField = ['id' => 'mod-login-username'];
+
+	/**
+	 * Locator for the Password field
+	 *
+	 * @var    array
+	 * @since  __DEPLOY_VERSION__
+	 */
+	public static $passwordField = ['id' => 'mod-login-password'];
+}
