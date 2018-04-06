434c9d6753f35b9b714a85cdde148761845c5141
David Neukirchen <david.neukirchen@gmail.com>
2018-04-06 16:15:48 +0200

    Fix namespace issue

--- src/_support/Page/Acceptance/JoomlaPage.php
+++ src/_support/Page/Acceptance/JoomlaPage.php
@@ -6,6 +6,9 @@
  * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
+namespace Page\Acceptance;
+
+use AcceptanceTester;
 
 /**
  * Acceptance Page object class to define joomla page objects.
