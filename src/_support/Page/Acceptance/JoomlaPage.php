1a2fd55aa38259f90df85b8465d9806add8bb909
David Neukirchen <david.neukirchen@gmail.com>
2018-04-06 16:13:39 +0200

    Docblock

--- src/_support/Page/Acceptance/JoomlaPage.php
+++ src/_support/Page/Acceptance/JoomlaPage.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * @package     Joomla.Test
+ * @subpackage  AcceptanceTester.Page
+ *
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
 
 /**
  * Acceptance Page object class to define joomla page objects.
