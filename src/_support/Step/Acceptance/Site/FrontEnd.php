463d698d7d8fcb09daac3d571ccf9c32fc6de678
Samgithub1997 <samarthsharma351@gmail.com>
2018-06-01 16:27:15 +0530

    category menu items

---
+++ src/_support/Step/Acceptance/Site/FrontEnd.php
@@ -0,0 +1,44 @@
+<?php
+    namespace Step\Acceptance\Site;
+
+    use Page\Acceptance\Site\FrontPage as FrontPage;
+
+    class FrontEnd extends \AcceptanceTester
+    {
+
+        /**
+         * Check whether Item is not Visible on front end
+         *
+         * @param \AcceptanceTester $I
+         * @param $menuItemName
+         *
+         * @return void
+         */
+        public static function notVisible(\AcceptanceTester $I, $menuItemName)
+        {
+
+            $I->comment('Make sure the menu item is not on site After unpublishing it');
+            $I->amOnPage(FrontPage::$url);
+
+            $I->dontSee($menuItemName);
+
+        }
+        /**
+         * Check whether Item is Visible on front end
+         *
+         * @param \AcceptanceTester $I
+         * @param $menuItemName
+         *
+         * @return void
+         */
+        public static function isVisible(\AcceptanceTester $I, $menuItemName)
+        {
+
+            $I->comment('Check the menu item on site After publishing it');
+            $I->amOnPage(FrontPage::$url);
+
+            $I->see($menuItemName);
+
+        }
+
+    }
