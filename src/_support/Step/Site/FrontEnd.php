9daa17b8a216a6cd838a13a78e17b179f0951be9
Samarth <Samarthsharma351@gmail.com>
2018-06-09 02:25:39 +0530

    Update FrontEnd.php

--- src/_support/Step/Site/FrontEnd.php
+++ src/_support/Step/Site/FrontEnd.php
@@ -40,5 +40,46 @@
             $I->see($menuItemName);
 
         }
+        /**
+         * Check whether Article is Visible on front end
+         *
+         * @param   \AcceptanceTester $I             Acceptance Tester
+         * @param   string            $menuItemName  Menu Item Name
+         * @param   string            $articleName   Name Of Article
+         *
+         * @return void
+         */
+        public static function articleIsVisible(\AcceptanceTester $I, $menuItemName, $articleName)
+        {
+
+            $I->comment('Check the menu item on site After publishing it');
+            $I->amOnPage(FrontPage::$url);
+
+            $I->see($menuItemName);
+            $I->click(['link' => $menuItemName]);
+            $I->see($articleName);
+
+        }
+        /**
+         * Check whether article is not Visible on front end
+         *
+         * @param   \AcceptanceTester $I             Acceptance Tester
+         * @param   string            $menuItemName  Menu Item Name
+         * @param   string            $articleName   Name Of Article
+         *
+         * @return void
+         */
+        public static function articleIsNotVisible(\AcceptanceTester $I, $menuItemName, $articleName)
+        {
+
+            $I->comment('Check the menu item on site After publishing it');
+            $I->amOnPage(FrontPage::$url);
+
+            $I->see($menuItemName);
+            $I->click(['link' => $menuItemName]);
+            $I->dontSee($articleName);
+
+        }
+
 
     }
