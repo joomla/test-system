30689ae81a4381bd65fbdda43c5340b73737a7fa
Samarth <Samarthsharma351@gmail.com>
2018-07-10 10:17:06 +0530

    Update MenuItemFormPage.php

--- src/_support/Page/Acceptance/Administrator/MenuItemFormPage.php
+++ src/_support/Page/Acceptance/Administrator/MenuItemFormPage.php
@@ -89,7 +89,7 @@ class MenuItemFormPage
 	 * @var    string
 	 * @since  4.0.0
 	 */
-		public static $archiveArticles = 'Archived Articles';
+	public static $archiveArticles = 'Archived Articles';
 
 	/**
 	 * Success Message
